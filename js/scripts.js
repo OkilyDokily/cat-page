e2ea53097b6b21417cd59fb454c14e6fb86b7d58
me <me@me.me.com>
2020-03-27 13:24:34 -0700

    use a toggle class to switch from light and dark

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
   $("#kittenimg").click(function(){
     $("#kittenimg").fadeToggle();
     $("#kittenmessage").fadeToggle();
-  })
+  });
 
   $("#kittenmessage").click(function(){
     $("#kittenimg").fadeToggle();
     $("#kittenmessage").fadeToggle();
-  })
+  });
 
+  $("#darktoggle").click(function(){
+    $("body").toggleClass("brightbody");
+    $("body").toggleClass("darkbody");
+  
+    $("#asyoucansee").toggleClass("light");
+    $("#asyoucansee").toggleClass("dark");
+    
+    $("#list").toggleClass("lightlist");
+    $("#list").toggleClass("darklist");
+  });
 
 });
